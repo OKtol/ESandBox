35adac0b9fe842cec0c8ebb4650a602938104756
OKtol <oktol.hz@gmail.com>
2022-12-12 18:39:46 +0300

    added window functionality

--- ESandBox/MVVM/ViewModel/MainViewModel.cs
+++ ESandBox/MVVM/ViewModel/MainViewModel.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using ESandBox.Core;
 
 namespace ESandBox.MVVM.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : ObservableObject
     {
+        private object _currentView;
+
+        public MainViewModel()
+        {
+            Application.Current.MainWindow.MaxWidth = SystemParameters.MaximizedPrimaryScreenHeight;
+            MoveWindowCommand = new RelayCommand(o => { Application.Current.MainWindow.DragMove(); });
+            ShutDownWindowCommand = new RelayCommand(o => { Application.Current.Shutdown(); });
+            MaximizeWindowCommand = new RelayCommand(o =>
+            {
+                if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+                    Application.Current.MainWindow.WindowState = WindowState.Normal;
+                else
+                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            });
+            MinimizeWindowCommand = new RelayCommand(o => {
+                Application.Current.MainWindow.WindowState = WindowState.Minimized; 
+            });
+        }
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #region Commands
+        public RelayCommand MoveWindowCommand { get; set; }
+        public RelayCommand ShutDownWindowCommand { get; set; }
+        public RelayCommand MaximizeWindowCommand { get; set; }
+        public RelayCommand MinimizeWindowCommand { get; set; }
+
+        #endregion
     }
 }
